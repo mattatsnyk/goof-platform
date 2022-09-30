0a56b8ac5dc081b2e46ad3f04e5bccbfac613a21
mattatsnyk <103199951+mattatsnyk@users.noreply.github.com>
2022-09-30 14:54:19 -0400

    Update app.js

--- Code/app.js
+++ Code/app.js
@@ -2,6 +2,7 @@
  * Copyright (c) 2014-2020 Bjoern Kimminich.
  * SPDX-License-Identifier: MIT
  */
+let password = "my secret password" 
 
 require('./lib/startup/validateDependencies')().then(() => {
   const server = require('./server')
